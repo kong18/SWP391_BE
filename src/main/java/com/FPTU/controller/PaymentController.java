54c40c73ecf5fb0b150f17ef9f205793bb060bf9
SonHo <hhson365@gmail.com>
2023-10-20 02:35:52 +0700

    fix user

--- src/main/java/com/FPTU/controller/PaymentController.java
+++ src/main/java/com/FPTU/controller/PaymentController.java
@@ -8,11 +8,16 @@ import com.FPTU.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RestController
 @RequestMapping("/payment")
@@ -31,9 +36,9 @@ public class PaymentController {
     }
 
     @PostMapping("/pay")
-    public String pay(HttpServletRequest request, @RequestParam("price") double price) {
-        String cancelUrl = Utils.getBaseURL(request) + "/" + URL_PAYPAL_CANCEL;
-        String successUrl = Utils.getBaseURL(request) + "/" + URL_PAYPAL_SUCCESS;
+    public ResponseEntity<?> pay(HttpServletRequest request, @RequestParam("price") double price) {
+        String cancelUrl = "http://127.0.0.1:5173/" +  URL_PAYPAL_CANCEL;
+        String successUrl = "http://127.0.0.1:5173/" + URL_PAYPAL_SUCCESS;
 
         try {
             Payment payment = paypalService.createPayment(
@@ -48,13 +53,22 @@ public class PaymentController {
 
             for (Links links : payment.getLinks()) {
                 if (links.getRel().equals("approval_url")) {
-                    return links.getHref();
+                    // Return success response to the front-end
+                    Map<String, String> response = new HashMap<>();
+                    response.put("success", Boolean.toString(true));
+                    response.put("successUrl", links.getHref());
+
+                    return ResponseEntity.ok().body(response);
                 }
             }
         } catch (PayPalRESTException e) {
             log.error(e.getMessage());
         }
-        return "redirect:/";
+        // Return error response to the front-end
+        Map<String, String> response = new HashMap<>();
+        response.put("success", Boolean.toString(false));
+
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping(URL_PAYPAL_CANCEL)
@@ -62,16 +76,20 @@ public class PaymentController {
         return "cancel";
     }
 
-    @GetMapping(URL_PAYPAL_SUCCESS)
-    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
-        try {
-            Payment payment = paypalService.executePayment(paymentId, payerId);
-            if (payment.getState().equals("approved")) {
-                return "success";
-            }
-        } catch (PayPalRESTException e) {
-            log.error(e.getMessage());
-        }
-        return "redirect:/";
-    }
+//    @GetMapping(URL_PAYPAL_SUCCESS)
+//    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+//        try {
+//            Payment payment = paypalService.executePayment(paymentId, payerId);
+//            if (payment.getState().equals("approved")) {
+//                return "success";
+//            }
+//        } catch (PayPalRESTException e) {
+//            log.error(e.getMessage());
+//        }
+//        return "redirect:/";
+//    }
+
+
+
+
 }
--- src/main/java/com/FPTU/model/User.java
+++ src/main/java/com/FPTU/model/User.java
@@ -64,23 +64,5 @@ public class User {
         return "/user-photos/" + userId + "/" + img;
     }
 
-  @OneToMany(mappedBy = "user")
-  private List<OrderCourse> orderCourses = new ArrayList<>();
-
-  @OneToMany(mappedBy = "user")
-  private List<OrderItem> orderItems = new ArrayList<>();
-
-  @OneToMany(mappedBy = "user",orphanRemoval = true)
-  private List<Course> courses = new ArrayList<>();
-
-  @OneToMany(mappedBy = "user")
-  private List<Rating> ratings = new ArrayList<>();
-
-  @OneToMany(mappedBy = "user")
-  private List<Comment> comments = new ArrayList<>();
-
-  private String img;
-
-  private String address;
 
 }
